161101069cf97ed3b1dde95182de63c9fb8a06db
Nourk4441 <nourk4441@gmail.com>
2024-12-13 14:45:02 +0200

    url endpoints

--- Taskgenda/taskmanager/views.py
+++ Taskgenda/taskmanager/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Task
+import json
 
-# Create your views here.
+@csrf_exempt
+def task_list(request):
+    if request.method == 'GET':
+        tasks = list(Task.objects.values())
+        return JsonResponse({'tasks': tasks}, safe=False)
+
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        task = Task.objects.create(
+            title=data.get('title'),
+            completed=data.get('completed', False)
+        )
+        return JsonResponse({'task': {'id': task.id, 'title': task.title}}, status=201)
+
+
