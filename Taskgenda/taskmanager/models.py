692990a84701d6a3cdef8122dc455d2ac49e4dc0
Nourk4441 <nourk4441@gmail.com>
2024-12-14 13:55:38 +0200

    authentication token

--- Taskgenda/taskmanager/models.py
+++ Taskgenda/taskmanager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- Taskgenda/taskmanager/views.py
+++ Taskgenda/taskmanager/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Task
+import json
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+@csrf_exempt
+def task_list(request):
+    if request.method == 'GET':
+        tasks = list(Task.objects.values())
+        return JsonResponse({'tasks': tasks}, safe=False)
+
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        task = Task.objects.create(
+            title=data.get('title'),
+            completed=data.get('completed', False)
+        )
+        return JsonResponse({'task': {'id': task.id, 'title': task.title}}, status=201)
